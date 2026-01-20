23f9753f32684dba1075ec36dea5c709f1ea6e7f
Ananya Jain <ananyaj1515@gmail.com>
2026-01-20 13:02:52 +0800

    Add Scanner to accept and echo user input

--- src/main/java/AstridGlowspell.java
+++ src/main/java/AstridGlowspell.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
+
 public class AstridGlowspell {
     public static void main(String[] args) {
-        String divider = "°. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.";
-        String greeting = "Hi Starlight, I am Astrid Glowspell! The planets were gossiping and your name came up";
-        String farewell = "Until our planets align again, may your transits be gentle";
+        String divider = "\t°. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.";
+        String greeting = "\tHi Starlight, I am Astrid Glowspell! The planets were gossiping and your name came up";
+        String farewell = "\tUntil our planets align again, may your transits be gentle";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(divider);
         System.out.println(greeting);
         System.out.println(divider);
+
+        String input = sc.nextLine();
+        while (!(input.equals("bye"))) {
+            System.out.println(divider);
+            System.out.println("\t" + input);
+            System.out.println(divider);
+            input = sc.nextLine();
+        }
+
+        System.out.println(divider);
         System.out.println(farewell);
         System.out.println(divider);
+        sc.close();
     }
 }
