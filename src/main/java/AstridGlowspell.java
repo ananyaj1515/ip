e720cbb93a561eee6c0a344f802d140e51942832
Ananya Jain <ananyaj1515@gmail.com>
2026-01-20 12:43:26 +0800

    Implement Greeting and Farewell

---
+++ src/main/java/AstridGlowspell.java
@@ -0,0 +1,13 @@
+public class AstridGlowspell {
+    public static void main(String[] args) {
+        String divider = "°. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.";
+        String greeting = "Hi Starlight, I am Astrid Glowspell! The planets were gossiping and your name came up";
+        String farewell = "Until our planets align again, may your transits be gentle";
+
+        System.out.println(divider);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(farewell);
+        System.out.println(divider);
+    }
+}
