24bb25d25a6df1975fdddd7385cc8d0baab679a4
Ananya Jain <ananyaj1515@gmail.com>
2026-01-20 15:45:58 +0800

    Add marking and unmarking of tasks
    Users cannot indicate whether the task is completed or not
    
    Let's add:
    * A Task class

--- src/main/java/AstridGlowspell.java
+++ src/main/java/AstridGlowspell.java
@@ -4,36 +4,65 @@ import java.util.Scanner;
 
 public class AstridGlowspell {
     public static void main(String[] args) {
+
+        // declarations
         String divider = "\t°. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.";
         String greeting = "\tHi Starlight, I am Astrid Glowspell! The planets were gossiping and your name came up";
         String farewell = "\tUntil our planets align again, may your transits be gentle";
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
+        // greetings
         System.out.println(divider);
         System.out.println(greeting);
         System.out.println(divider);
 
+        // get user input
         String input = sc.nextLine();
+
         while (!(input.equals("bye"))) {
             System.out.println(divider);
+
+            // view all tasks
             if (input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("\t %d. %s\n", i + 1,tasks.get(i));
+                    System.out.printf("\t %d. %s\n", i + 1,tasks.get(i).toString());
                 }
             }
-            else {
-                tasks.add(input);
-                System.out.println("\t added: " + input);
+
+            // mark task as done
+            else if (input.startsWith("mark")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks.get(index - 1).markAsDone();
+                System.out.println("\t Nice I've marked this task as done:");
+                System.out.printf("\t\t %s\n", tasks.get(index - 1).toString());
             }
 
+            // unmark task as done
+            else if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks.get(index - 1).markAsUndone();
+                System.out.println("\t OK, I've marked this task as not done yet:");
+                System.out.printf("\t\t %s\n", tasks.get(index - 1).toString());
+            }
+
+            // add new task
+            else  {
+                tasks.add(new Task(input));
+                System.out.println("\t added: " + input);
+            }
             System.out.println(divider);
+
+            // take next command
             input = sc.nextLine();
         }
 
+        // farewell
         System.out.println(divider);
         System.out.println(farewell);
         System.out.println(divider);
+
+        // closing the scanner
         sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String desc;
+    protected boolean isDone;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    @Override
+    public String toString() {
+        return this.getStatus() + " " + this.desc;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
